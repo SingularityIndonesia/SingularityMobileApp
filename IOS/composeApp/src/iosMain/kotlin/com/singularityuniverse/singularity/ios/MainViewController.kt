3f43ce260a5da7dc32b957c5a1a224f77009d3bf
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-01 10:11:46 +0700

    fix ios setup

--- IOS/composeApp/src/iosMain/kotlin/com/singularityuniverse/singularity/ios/MainViewController.kt
+++ IOS/composeApp/src/iosMain/kotlin/com/singularityuniverse/singularity/ios/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.singularityuniverse.singularity.ios
 
+import App
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun MainViewController() = ComposeUIViewController { App() }
