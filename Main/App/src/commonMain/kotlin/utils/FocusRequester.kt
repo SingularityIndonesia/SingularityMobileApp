db2312e76f562d505c15a705faa46539e1ae59a2
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-08 21:13:48 +0700

    better abstraction

--- Main/App/src/commonMain/kotlin/utils/FocusRequester.kt
+++ Main/App/src/commonMain/kotlin/utils/FocusRequester.kt
@@ -1,28 +1,11 @@
 package utils
 
 import androidx.compose.ui.focus.FocusRequester
-import io.ktor.util.date.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
 
 suspend fun FocusRequester.requestFocus(timeout: Duration): Boolean {
-    val timeoutMillis = timeout.inWholeMilliseconds // 2 seconds max
-    val tic = getTimeMillis()
-    val toc = { getTimeMillis() - tic }
-    var error = ""
-
-    while (coroutineContext.isActive && (toc() < timeoutMillis)) {
-        runCatching { requestFocus() }
-            .onFailure { error = "Error: requestFocus ${it.message ?: it.cause?.message ?: it}" }
-            .onSuccess {
-                println("Success: requestFocus")
-                return true
-            }
-        delay(100)
+    temporaryLoop(timeout = timeout) {
+        requestFocus()
     }
-
-    println("Error: requestFocus TIMEOUT\n$error")
     return false
 }
---
+++ Main/App/src/commonMain/kotlin/utils/Loop.kt
@@ -0,0 +1,41 @@
+package utils
+
+import io.ktor.util.date.getTimeMillis
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlin.coroutines.coroutineContext
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+sealed class TemporaryLoopException : Exception() {
+    data class UnknownException(override val message: String) : TemporaryLoopException()
+    data class OperationFailureException(override val cause: Throwable) : TemporaryLoopException()
+}
+
+suspend fun <T> temporaryLoop(
+    timeout: Duration,
+    delay: Duration = 100.milliseconds,
+    block: suspend () -> T
+): Result<T> {
+    val timeoutMillis = timeout.inWholeMilliseconds // 2 seconds max
+    val delayMillis = delay.inWholeMilliseconds
+
+    val tic = getTimeMillis()
+    val toc = { getTimeMillis() - tic }
+    var result: Result<T> = Result.failure(TemporaryLoopException.UnknownException("Execution not yet invoked"))
+
+    while (coroutineContext.isActive && (toc() < timeoutMillis)) {
+        runCatching { block.invoke() }
+            .onFailure {
+                result = Result.failure(TemporaryLoopException.OperationFailureException(it))
+            }
+            .onSuccess {
+                result = Result.success(it)
+                break
+            }
+
+        delay(delayMillis)
+    }
+
+    return result
+}
