c529b982922602e102aa104fb111d365910114ec
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-13 03:41:23 +0700

    login: differentiate state

--- Main/App/src/commonMain/kotlin/ui/screen/login/LoginScreenState.kt
+++ Main/App/src/commonMain/kotlin/ui/screen/login/LoginScreenState.kt
@@ -4,6 +4,6 @@ data class LoginScreenState(
     val email: String = "",
     val isLoading: Boolean = false,
     val emailError: String? = null,
-    val isTextInputEnabled: Boolean = false,
+    val isTextInputEnabled: Boolean = true,
     val isSubmitButtonEnabled: Boolean = false,
 )
