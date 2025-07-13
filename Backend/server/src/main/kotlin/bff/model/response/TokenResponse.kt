0112d9957e40ed88daf71bb374d70ef14228e6c5
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-13 13:00:07 +0700

    remove comment

--- Backend/server/src/main/kotlin/bff/model/response/TokenResponse.kt
+++ Backend/server/src/main/kotlin/bff/model/response/TokenResponse.kt
@@ -2,9 +2,6 @@ package bff.model.response
 
 import kotlinx.serialization.Serializable
 
-/**
- * Response data class for login OTP request
- */
 @Serializable
 data class TokenResponse(
     val token: String
