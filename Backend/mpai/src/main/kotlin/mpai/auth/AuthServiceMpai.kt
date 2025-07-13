dbf22896e9298e843693dadd0f065cced1c3efb0
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-13 17:33:00 +0700

    handle Errors punk way

--- Backend/mpai/src/main/kotlin/mpai/auth/AuthServiceMpai.kt
+++ Backend/mpai/src/main/kotlin/mpai/auth/AuthServiceMpai.kt
@@ -7,11 +7,15 @@ import model.form.FormStatus
 import model.form.FormType
 import model.form.LoginWithOtpForm
 import mpai.MPAI
+import utils.isValidEmail
 import java.util.*
 import kotlin.time.Duration.Companion.minutes
 
 suspend fun MPAI.requestLoginWithOtp(token: String, form: LoginWithOtpForm): Result<LoginWithOtpForm> {
     return runCatching {
+        check(form.body.email.isNotBlank())
+        check(isValidEmail(form.body.email))
+
         val newForm = form.copy(
             header = FormHeader(
                 id = UUID.randomUUID().toString(),
