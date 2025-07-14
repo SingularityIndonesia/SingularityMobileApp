9ad9cb08dea48915616d9093c894c96ee3cef0bd
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-14 13:35:28 +0700

    simplify

---
+++ Backend/app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlinxSerialization)
+    application
+}
+
+group = "com.singularityuniverse.memories.core"
+version = "1.0.0"
+
+application {
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.add("-Xcontext-parameters")
+        }
+    }
+}
+
+dependencies {
+    
+    api(libs.logback)
+    api(libs.ktor.serverCore)
+    api(libs.ktor.serverNetty)
+    api(libs.ktor.serverContentNegotiation)
+    api(libs.ktor.serializationKotlinxJson)
+
+    // Database dependencies
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.kotlinDatetime)
+    implementation(libs.exposed.json)
+    implementation(libs.postgresql)
+    implementation(libs.h2) // for testing
+    implementation(libs.hikari)
+
+    // Test dependencies
+    testImplementation(libs.ktor.serverTestHost)
+    testImplementation(libs.kotlin.testJunit)
+    testImplementation(libs.ktor.clientContentNegotiation)
+}
