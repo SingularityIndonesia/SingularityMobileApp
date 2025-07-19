e5f52f257ec94600bf23f7794c91030f736779a7
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-19 19:16:48 +0700

    update preview

--- Frontend/Main/Core/src/commonMain/kotlin/designsystem/component/experimental/VerticalDoubleRowFlow.kt
+++ Frontend/Main/Core/src/commonMain/kotlin/designsystem/component/experimental/VerticalDoubleRowFlow.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import designsystem.component.RatioImage
-import kotlinx.coroutines.delay
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.math.min
 
@@ -124,35 +123,29 @@ fun <T> defineYPositionRelative(
 @Composable
 fun VerticalDoubleRowFlowPreview() {
     val images = remember {
-        mutableStateListOf(
+        listOf(
             "https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=",
             "https://cnc-magazine.oramiland.com/parenting/images/kim-da-hyun.width-800.format-webp.webp",
             "https://burst.shopifycdn.com/photos/fashion-model-in-fur.jpg?width=1000&format=pjpg&exif=0&iptc=0",
             "https://cdn.shopify.com/s/files/1/0469/3927/5428/files/TWICE-Dahyun-for-A-pieu-2023-Juicy-Pang-Tint-documents-2.jpg?v=1738754206",
             "https://i.pinimg.com/736x/3f/10/a6/3f10a655fbe33402d858bffa7193df16.jpg",
-            "https://images.saatchiart.com/saatchi/2199861/art/10341725/9404341-JUYLYEKQ-32.jpg"
+            "https://images.saatchiart.com/saatchi/2199861/art/10341725/9404341-JUYLYEKQ-32.jpg",
+            "https://pbs.twimg.com/media/GllvhtPW4AAUnRR?format=jpg&name=large",
+            "https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?semt=ais_hybrid&w=740",
+            "https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg",
+            "https://media.istockphoto.com/id/2148595463/photo/african-elephant-in-wilderness-at-sunset.jpg?s=612x612&w=0&k=20&c=WmZfQYFpQOKTIyB95U6NQ6h8WQM-Pjp9TCMvF63-Xf4=",
+            "https://thumbs.dreamstime.com/b/i-m-hungry-ma-12739742.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmfuHcvQcxVfU6YzM_ty5dfnSmojk8h2NSoi58XxFOzbh1ceRx9En9nnZUiVNqhH6IX-M&usqp=CAU"
         )
     }
 
-    LaunchedEffect(Unit) {
-        delay(2000)
-        images.addAll(
-            listOf(
-                "https://pbs.twimg.com/media/GllvhtPW4AAUnRR?format=jpg&name=large",
-                "https://img.freepik.com/free-photo/closeup-scarlet-macaw-from-side-view-scarlet-macaw-closeup-head_488145-3540.jpg?semt=ais_hybrid&w=740",
-                "https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg",
-                "https://media.istockphoto.com/id/2148595463/photo/african-elephant-in-wilderness-at-sunset.jpg?s=612x612&w=0&k=20&c=WmZfQYFpQOKTIyB95U6NQ6h8WQM-Pjp9TCMvF63-Xf4=",
-                "https://thumbs.dreamstime.com/b/i-m-hungry-ma-12739742.jpg",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmfuHcvQcxVfU6YzM_ty5dfnSmojk8h2NSoi58XxFOzbh1ceRx9En9nnZUiVNqhH6IX-M&usqp=CAU"
-            )
-        )
-    }
+    val items = remember { (0..30).toList() }
 
     VerticalDoubleRowFlow(
         modifier = Modifier
             .systemBarsPadding()
             .fillMaxSize(),
-        items = images,
+        items = items,
         verticalGap = 4.dp,
         horizontalGap = 4.dp,
         contentPadding = PaddingValues(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
@@ -163,7 +156,7 @@ fun VerticalDoubleRowFlowPreview() {
                 .border(BorderStroke(1.px, Color.Black.copy(alpha = .5f)))
                 .padding(16.dp)
                 .fillMaxWidth(),
-            model = item
+            model = images.random()
         )
     }
 }
